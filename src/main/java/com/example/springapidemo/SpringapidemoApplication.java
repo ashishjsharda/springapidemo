28d35395bf9a428d5fd4ccfae37a1935950c838b
Ashish Sharda <ashishjsharda@gmail.com>
2023-05-11 06:58:38 -0500

    first commit

---
+++ src/main/java/com/example/springapidemo/SpringapidemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringapidemoApplication.class, args);
+    }
+
+}
